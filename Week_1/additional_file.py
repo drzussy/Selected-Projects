bcdc2395daa806eb957e0dfbbaae3ebc7779268a
drzussy <87827403+drzussy@users.noreply.github.com>
2023-09-19 15:55:23 +0300

    Delete Week_1 directory

--- Week_1/additional_file.py
+++
@@ -1,2 +0,0 @@
-def secret_function():
-    print("My username is 'noam.susman' and I found the string 'za1gDhcXG0Ur' in the submission response.")
--- Week_1/lab1.py
+++
@@ -1,8 +0,0 @@
-#############################################################
-# FILE : lab1.py
-# WRITER : noam susman , noam.susman , 318528304
-# EXERCISE : intro2cs1 lab1 2023
-# DESCRIPTION: A simple program that prints "Hello World!" to
-# the standard output (screen).
-#############################################################
-print("Hello World!")
